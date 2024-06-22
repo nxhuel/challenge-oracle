777549ebe72c1cf9ccdde663cea551fc782e0ceb
nxhuel <nahueltisera03@gmail.com>
2024-06-22 17:34:55 -0300

    Finalizando proyecto

--- app.js
+++ app.js
@@ -10,9 +10,8 @@
 // COPIAR TEXTO
 
 // ARREGLAR
-// id="textoMostrado" -> se rompe al momento de poner mucho texto
-// Hacerlo responsive
-// Subir un poco mas los botones de encriptar/desencriptar
+
+document.getElementById('textoMostrado').style.visibility = 'hidden';
 
 const asignarTextoId = (id, texto) => {
     let elementoHTML = document.getElementById(id);
@@ -21,6 +20,7 @@ const asignarTextoId = (id, texto) => {
 };
 
 const condicionesIniciales = () => {
+    document.querySelector('#textoEncriptado').value = '';
     asignarTextoId("imagen", "img/image.png");
     asignarTextoId("mostrarSubtitulo", "Ningun mensaje fue encontrado");
     asignarTextoId("mostrarTexto", "Ingrese el texto que desees escriptar o desencriptar.");
@@ -35,6 +35,27 @@ const obtenerTexto = () => {
 const encriptarTexto = () => {
     texto = obtenerTexto();
     console.log(texto);
+
+    if (texto == '') {
+        document.getElementById('textoMostrado').style.visibility = 'visible';
+        let msgErrorVacio = document.getElementById('textoMostrado');
+        msgErrorVacio.innerHTML = 'No dejes el campo vacío.'
+
+        document.getElementById('imagen').style.visibility = 'hidden';
+        document.getElementById('mostrarSubtitulo').style.visibility = 'hidden';
+        document.getElementById('mostrarTexto').style.visibility = 'hidden';
+
+        setTimeout(() => {
+            document.getElementById('textoMostrado').style.visibility = 'hidden';
+            document.getElementById('imagen').style.visibility = 'visible';
+            document.getElementById('mostrarSubtitulo').style.visibility = 'visible';
+            document.getElementById('mostrarTexto').style.visibility = 'visible';
+        }, 1000);
+
+        return;
+    };
+
+    document.getElementById('textoMostrado').style.visibility = 'visible';
     document.getElementById('imagen').style.visibility = 'hidden';
     document.getElementById('mostrarSubtitulo').style.visibility = 'hidden';
     document.getElementById('mostrarTexto').style.visibility = 'hidden';
@@ -42,6 +63,7 @@ const encriptarTexto = () => {
 
 const desencriptarTexto = () => {
     condicionesIniciales();
+    document.getElementById('textoMostrado').style.visibility = 'hidden';
     document.getElementById('imagen').style.visibility = 'visible';
     document.getElementById('mostrarSubtitulo').style.visibility = 'visible';
     document.getElementById('mostrarTexto').style.visibility = 'visible';
@@ -49,10 +71,28 @@ const desencriptarTexto = () => {
 
 const copiarTexto = () => {
     texto = obtenerTexto();
+
     navigator.clipboard.writeText(texto).then(function () {
         console.log('Texto copiado al portapapeles');
     }).catch(function (err) {
         console.log('Error al copiar el texto: ', err);
     });
+
+    let textoCopiado = document.getElementById('textoCopiado');
+
+    if (texto != '') {
+        textoCopiado.innerHTML = 'Texto copiado';
+
+        setTimeout(() => {
+            textoCopiado.innerHTML = 'Copiar';
+        }, 1000)
+    } else {
+        textoCopiado.innerHTML = 'No se registro nada para copiar';
+    }
+
+    setTimeout(() => {
+        textoCopiado.innerHTML = 'Copiar';
+    }, 1000)
+
 };
 
